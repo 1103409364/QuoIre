d0edfda7bda6caceca016b37a89a4cc5a94f8989
1103409364 <1103409364@qq.com>
2019-07-21 21:42:15 +0800

    project init

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,17 +1,14 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    hello world
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'Home'
 }
 </script>
 
 <style>
-#app {
-  color: #2c3e50;
-}
 </style>
