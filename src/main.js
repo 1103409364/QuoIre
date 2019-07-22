fca90287664fa7cf9dc09901f46778fae526a658
1103409364 <1103409364@qq.com>
2019-07-22 16:04:28 +0800

    change

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 // 使用 styles 别名, 在 webpack.base.conf.js 中配置
 import 'styles/reset.css'
 import 'styles/border.css'
@@ -11,6 +13,7 @@ import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper /* { default global options } */)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="wrapper">
+  <swiper :options="swiperOption">
+    <!-- slides -->
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+      <img class="swiper-img" :src=item.imgUrl alt="p">
+    </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        // 图片分页小点, 传 class 进去
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: ' 01',
+        imgUrl: 'http://source.qunarzz.com/site/images/wns/20190626_dujia_homepage_top_banner_5.jpg'
+      }, {
+        id: '02',
+        imgUrl: 'http://source.qunarzz.com/site/images/wns/2019715_dujia_homepage_top_banner_6.jpg'
+      }]
+    }
+  }
+
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+  background-color #fff
+.wrapper
+  overflow hidden
+  width 100%
+  height 0
+  padding-bottom: 25.60%
+  background-color: #999
+  .swiper-img
+    width 100%
+</style>
