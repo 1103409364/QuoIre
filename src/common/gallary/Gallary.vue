1d6f475b1e0b396d8832f4afc6920e4c8ddd52de
1103409364 <1103409364@qq.com>
2019-07-25 10:56:26 +0800

    banner finish

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="container" @click="handleGallaryClick">
+    <div class="wrapper">
+      <swiper :options="swiperOptions">
+        <!-- slides -->
+        <swiper-slide v-for="(item, index) in imgs" :key="index">
+          <img class="swiper-img" :src="item" alt="p">
+        </swiper-slide>
+        <!-- Optional controls -->
+        <div class="swiper-pagination"  slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CommonGallary',
+  props: {
+    imgs: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+      swiperOptions: {
+        pagination: '.swiper-pagination',
+        // 分页器样式
+        paginationType: 'fraction',
+        // 切换效果
+        effect: 'coverflow',
+        // 监听到父级元素 dom 结构变化自我刷新,解决 swiper 宽度计算的问题
+        observeParents: true,
+        // 监听到这个元素结构变化自我刷新
+        observer: true
+      }
+    }
+  },
+  methods: {
+    // 画廊点击的时候发布关闭事件
+    handleGallaryClick () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .container >>> .swiper-container
+    overflow inherit
+  .container
+    display flex
+    flex-direction column
+    justify-content center
+    z-index 99
+    position fixed
+    left 0
+    right 0
+    top 0
+    bottom 0
+    background-color #000
+    .wrapper
+      // overflow hidden
+      height 0
+      width 100%
+      padding-bottom 68.6%
+      background-color #fff
+      .swiper-img
+        width 100%
+      .swiper-pagination
+        color #fff
+        bottom -1rem
+</style>
