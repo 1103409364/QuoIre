bc380533b8c3439ff9db0edfe3ec43fdd17d87a2
1103409364 <1103409364@qq.com>
2019-07-25 15:44:55 +0800

    keep scrollTop

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -28,6 +28,7 @@ export default {
   data () {
     return {
       lastCity: '', // 上一次的城市
+      scrollTop: 0,
       swiperList: [],
       iconList: [],
       recommendList: [],
@@ -53,20 +54,35 @@ export default {
         this.recommendList = data.recommendList
         this.weekendList = data.weekendList
       }
+    },
+    handleScroll () {
+      this.scrollTop = document.documentElement && document.documentElement.scrollTop
     }
   },
   mounted () {
     this.getHomeInfo()
     // city 从计算属性中来
     this.lastCity = this.city
+    //
+    window.addEventListener('scroll', this.handleScroll)
   },
-  // 使用 keep-alive 之后, 组件会多一个生命周期 activated
+  // 使用 keep-alive 之后, 组件会多一个生命周期 activated, keep-alive加载时调用
   activated () {
     // 当城市发生改变的时候，请求新的数据
     if (this.lastCity !== this.city) {
       this.lastCity = this.city
       this.getHomeInfo()
     }
+    // 进入详情页记住滚动位置
+    if (this.scrollTop > 0) {
+      window.scrollTo(0, this.scrollTop)
+      this.scrollTop = 0
+      window.addEventListener('scroll', this.handleScroll)
+    }
+  },
+  // deactivated 页面退出时关闭事件 防止其他页面出现问题
+  deactivated () {
+    window.removeEventListener('scroll', this.handleScroll)
   }
 }
 </script>
